6dba50c0bb72d13f182f0b7f0926c56d92b58c89
Bancy614 <wambui.bancy@gmail.com>
2023-07-26 09:52:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 let apikey = "33f2tedfbf4o930695dbbb808ab0c1ae";
 let city = "Nairobi";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+console.log(apiUrl);
 
 // https://api.shecodes.io/weather/v1/current?query=Nairobi&key=33f2tedfbf4o930695dbbb808ab0c1ae&units=metric
 
 function displayTemperature(response) {
-  console.log(response);
-  // let cityElement = document.querySelector("#city");
+  console.log(response.data);
+  let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let temperatureElement = document.querySelector("#temperature");
   let humidityElement = document.querySelector("#humidity");
   lewindElement = document.querySelector("#wind");
 
-  // cityElement.innerHTML = response.data.name;
+  cityElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = response.data.main.temp;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
