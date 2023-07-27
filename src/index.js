6c80f95291daf805855e8c17cd19fe52349ac4bf
Bancy614 <wambui.bancy@gmail.com>
2023-07-27 07:36:43 +0300

    updated time

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 let apikey = "33f2tedfbf4o930695dbbb808ab0c1ae";
 let city = "Nairobi";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-console.log(apiUrl);
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=33f2tedfbf4o930695dbbb808ab0c1ae&units=metric`;
 
 // https://api.shecodes.io/weather/v1/current?query=Nairobi&key=33f2tedfbf4o930695dbbb808ab0c1ae&units=metric
 
@@ -11,12 +10,14 @@ function displayTemperature(response) {
   console.log(response.data.condition.description);
   console.log(response.data.temperature.humidity);
   console.log(response.data.wind.speed);
+  console.log(response.data.time);
 
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
 
   cityElement.innerHTML = response.data.city;
   let temperature = Math.round(response.data.temperature.current);
@@ -25,5 +26,24 @@ function displayTemperature(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   let wind = Math.round(response.data.wind.speed);
   windElement.innerHTML = `${wind}Km/h`;
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 axios.get(apiUrl).then(displayTemperature);
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+
+  let day = date.toLocaleString("default", {
+    weekday: "long",
+  });
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
